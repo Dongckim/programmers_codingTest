c7848b7acec6fd37e995d547961edc4118c0220e
dongckim <dck.alx@gmail.com>
2023-05-18 23:50:56 +0900

    기본 세팅 완료

---
+++ 알고리즘스터디/1764/1764.js
@@ -0,0 +1,7 @@
+let input = require('fs').readFileSync('input.txt').toString().split('\n');
+console.log(input);
+
+
+// function solution(s){
+    
+// }
--- 알고리즘스터디/1764/1764듣보잡.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString();
-
-console.log(input);
-
-
-function solution(s){
-    
-}
