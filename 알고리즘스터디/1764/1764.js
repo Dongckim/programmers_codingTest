6a11b93f4a6a2146adaba991635675f244929665
dongckim <dck.alx@gmail.com>
2023-05-21 22:30:28 +0900

    1764 문제해결

--- 알고리즘스터디/1764/1764.js
+++ 알고리즘스터디/1764/1764.js
@@ -1,6 +1,48 @@
 const input = require('fs').readFileSync('input.txt').toString().split('\n');
 
-function solution(input){
-    
+function solution1(input){
+    const NohearSeen = new Set();
+    input.map(item => NohearSeen.add(item))
 
+    let DeleteElement = Array.from(NohearSeen).filter((item,index) => item !== input[index])
+    let NohearSeenElement = input.filter((item,index) => item !== Array.from(NohearSeen)[index]).filter(item => !DeleteElement.includes(item))
+
+    return [NohearSeenElement.length, ...NohearSeenElement]
 }
+
+function solution2(input){
+    const [NoHear, NoSeen] = input[0].split(' ')
+    let NoHearLst = input.slice(1, +NoHear+1)
+    let NoSeenLst = input.slice(NoSeen)
+
+    let answer = NoHearLst.filter(item => {
+        for(let data of NoSeenLst){
+            if(data === item){
+                return true
+            }
+            else{
+                return false
+            }
+        }
+    })
+    answer.unshift(answer.length)
+    return answer
+}
+
+function solution3(input){
+    const [NoHear, NoSeen] = input[0].split(' ')
+    const HearSet = new Set();
+    const SeenSet = new Set();
+    for(let i = 1; i < input.length; i++){
+        if(i < +NoHear+1){
+            HearSet.add(input[i])
+        }
+        else{
+            SeenSet.add(input[i])
+        }
+    }
+    let answer = []
+    Array.from(HearSet).map(item => SeenSet.has(item) ? answer.push(item) : null)
+    return answer.length + '\n' +  answer.sort().join('\n')
+}
+console.log(solution3(input))
