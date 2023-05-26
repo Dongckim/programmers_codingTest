2bc8e9860906abd61b018a95f752b026ddc0082b
Dongchan Alex Kim <123378711+Dongckim@users.noreply.github.com>
2023-05-27 01:21:14 +0900

    [Silver III] Title: 서강근육맨, Time: 192 ms, Memory: 11680 KB -BaekjoonHub

---
+++ 백준/Silver/20300. 서강근육맨/서강근육맨.js
@@ -0,0 +1,25 @@
+const path = process.platform === 'linux' ? "/dev/stdin" : "input.txt";
+const [num, mLoss] = require('fs').readFileSync(path).toString().split('\n');
+
+function solution1(num, mLoss){
+    mLoss = mLoss.split(' ').map(data => BigInt(data)).sort((a, b) => (a < b ? -1 : 1));
+    // return mLoss.map(data => String(data))
+    let answer = 0
+    if(num % 2 === 0){
+        for(let i = 0; i < num-1; i++){
+            let sum = mLoss[i]+mLoss[num-(i+1)]
+            answer = answer > sum ? answer :  sum
+        }
+        return String(answer)
+    }
+    else{
+        answer = mLoss[num-1]
+        for(let i = 0; i < num-2; i++){
+            let sum = mLoss[i]+mLoss[num-(i+2)]
+            answer = answer > sum ? answer :  sum
+        }
+        return String(answer)
+    }
+}
+
+console.log(solution1(num, mLoss))
