32b05074ea453efd965617eb84c761c66d5e22aa
Alex <dck.alx@gmail.com>
2023-12-30 23:59:30 +0900

    [CLASS02 중간커밋] 2023.12.30.

--- 백준/CLASS02/1929.py
+++ 백준/CLASS02/1929.py
@@ -1,12 +1,13 @@
 k, m = list(map(int, open('input.txt', 'rt').read().split(' ')))
 
 def find_prime(num):
-    for i in range(2, num):
+    if num == 1:
+            return False
+    for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
             return False
     return True
 
-for i in range(k, m):
+for i in range(k, m+1):
     if find_prime(i):
         print(i)
-
