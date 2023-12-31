12e98ed8d5a921e163392945a0d9214c94249a5a
Alex <dck.alx@gmail.com>
2023-12-31 22:21:02 +0900

    [SILVER02 해결] 2023.12.31.

--- 백준/CLASS02/2874.py
+++ 백준/CLASS02/2874.py
@@ -6,12 +6,26 @@ stack = []
 answer = []
 
 for i in range(num):
-    if len(stack) == 0:
+    if len(stack_ls) == 0:
         pass
-    if stack_ls[i] != origin[i]: #오리진이랑 다르면
-        stack.insert(0, stack_ls[i])
-        
-    else:   #오리진이랑 같으면, 
-        if len(stack) != 0:
-            answer.append(stack.pop(0))
-print(stack_ls)
+    else:
+        stack.insert(0, origin[i])
+        answer.append('+')
+        while True:
+            try:
+                if stack[0] == stack_ls[0]:
+                        stack_ls.pop(0)
+                        stack.pop(0)
+                        answer.append('-') 
+                elif stack[0] > stack_ls[0]:
+                    answer.append('NO') 
+                    break
+                else:
+                    break
+            except:
+                 break
+    
+if 'NO' in answer:
+     print('NO')
+else:
+    [print(i) for i in answer]
